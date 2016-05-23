a0fea1175f455e2b8ca3a23f0f2b0caab6f4d882
Phil Kunz <github@philkunz.com>
2016-05-23 07:56:09 +0200

    fix imports

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,4 @@
-import "globals/colors/index.d.ts"
-import "globals/mocha/index.d.ts"
-import "globals/node/index.d.ts"
-import "globals/vinyl/index.d.ts"
+import "./globals/colors/index.d.ts"
+import "./globals/mocha/index.d.ts"
+import "./globals/node/index.d.ts"
+import "./globals/vinyl/index.d.ts"

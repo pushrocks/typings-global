62df2d95991f11cdd5e73ed22a6b154b65b30d18
Nicholas Iannone <nickiannone@gmail.com>
2016-09-01 19:19:46 +0000

    Update install.js; fix ES5 compat errors.

--- install.js
+++ install.js
@@ -1,12 +1,14 @@
+'use strict';
+
 var shelljs = require("shelljs");
 var semver = require("semver");
 
-let nodeVersion = shelljs.exec("node -v",{silent:true}).stdout;
+var nodeVersion = shelljs.exec("node -v",{silent:true}).stdout;
 
 var sanatizedNodeVersion = /\n?v(.*)\n?\s*$/.exec(nodeVersion)[1];
 console.log("Your node version is " + sanatizedNodeVersion);
 console.log("Trying to install node typings in matching version...");
 
-nodeMajorVersion = semver.major(sanatizedNodeVersion);
-nodeNextVersion = nodeMajorVersion + 1;
+var nodeMajorVersion = semver.major(sanatizedNodeVersion);
+var nodeNextVersion = nodeMajorVersion + 1;
 shelljs.exec('npm install @types/node@">=' + nodeMajorVersion + '.0.0 <' +  nodeNextVersion + '.0.0"');

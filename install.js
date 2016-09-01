bec6902d42ce1fb257618514db2b70e2858465fd
PhilKunz <github@philkunz.com>
2016-09-01 19:03:46 +0200

    fix major version selection

--- install.js
+++ install.js
@@ -1,8 +1,11 @@
 var shelljs = require("shelljs");
+var semver = require("semver");
 
 let nodeVersion = shelljs.exec("node -v",{silent:true}).stdout;
 
 var sanatizedNodeVersion = /\n?v(.*)\n?\s*$/.exec(nodeVersion)[1];
 console.log("Your node version is " + sanatizedNodeVersion);
 console.log("Trying to install node typings in matching version...");
-shelljs.exec("npm install @types/node@>=" + sanatizedNodeVersion);
+
+nodeMajorVersion = semver.major(sanatizedNodeVersion);
+shelljs.exec('npm install @types/node@">=' + nodeMajorVersion + '.0.0"');

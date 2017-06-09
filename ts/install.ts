7e2b8a5a578d3a91a64d6d38b7ef61e94616b2c9
Phil Kunz <gitlab@philkunz.com>
2017-06-09 21:32:00 +0200

    update to always install a working version

--- ts/install.ts
+++ ts/install.ts
@@ -1,24 +1,30 @@
 //declare let require;
-var shelljs = require("shelljs");
+import * as smartshell from 'smartshell'
 var semver = require("semver");
 
-var nodeVersion = shelljs.exec("node -v", { silent: true }).stdout;
+let safeInstall = async () => {
+    var nodeVersion = (await smartshell.execSilent("node -v")).stdout;
 
-//check for existing node typings
-let treeLocal = shelljs.exec("(npm list)", { silent: true }).stdout;
-let treeOuter = shelljs.exec("(cd ../ && npm list)", { silent: true }).stdout;
+    //check for existing node typings
+    let treeLocal = (await smartshell.execSilent("(npm list)")).stdout
+    let treeOuter = (await smartshell.execSilent("(cd ../ && npm list)")).stdout;
 
-let typesNodeRegex = /\@types\/node/
+    let typesNodeRegex = /\@types\/node/
 
-if (typesNodeRegex.test(treeLocal) || typesNodeRegex.test(treeOuter)) {
-    console.log("@types/node already installed") !
-} else {
-    console.log("@types/node not yet installed");
-    var sanatizedNodeVersion = /\n?v(.*)\n?\s*$/.exec(nodeVersion)[1];
-    console.log("Your node version is " + sanatizedNodeVersion);
-    console.log("Trying to install node typings in matching version...");
+    if (typesNodeRegex.test(treeLocal) || typesNodeRegex.test(treeOuter)) {
+        console.log("@types/node already installed")!
+    } else {
+        console.log("@types/node not yet installed");
+        var sanatizedNodeVersion = /\n?v(.*)\n?\s*$/.exec(nodeVersion)[ 1 ];
+        console.log("Your node version is " + sanatizedNodeVersion);
+        console.log("Trying to install node typings in matching version...");
 
-    var nodeMajorVersion = semver.major(sanatizedNodeVersion);
-    var nodeNextVersion = nodeMajorVersion + 1;
-    shelljs.exec(`cd .. && npm install @types/node@">=${nodeMajorVersion}.0.0 <${nodeNextVersion}.0.0"`);
+        var nodeMajorVersion = semver.major(sanatizedNodeVersion);
+        var nodeNextVersion = nodeMajorVersion + 1;
+        let exitCode = (await smartshell.exec(`cd .. && npm install @types/node@">=${nodeMajorVersion}.0.0 <${nodeNextVersion}.0.0"`)).exitCode
+        if (exitCode !== 0) {
+            await smartshell.exec('npm install @types/node')
+        }
+    }
 }
+safeInstall()
